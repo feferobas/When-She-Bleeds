76a31133054e3f6ce022dc5a13c3600a6ff6a5bf
Fernanda <fefematsumoto2006@gmail.com>
2023-07-20 10:50:11 -0300

    Initial Commit

---
+++ finalProjectKWK/menstruationCycleStartPortView.swift
@@ -0,0 +1,20 @@
+//
+//  menstruationCycleStartPortView.swift
+//  finalProjectKWK
+//
+//  Created by Cristiane Andrade on 19/07/23.
+//
+
+import SwiftUI
+
+struct menstruationCycleStartPortView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct menstruationCycleStartPortView_Previews: PreviewProvider {
+    static var previews: some View {
+        menstruationCycleStartPortView()
+    }
+}
